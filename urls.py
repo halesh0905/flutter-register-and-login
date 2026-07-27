cf469ba10858e8189dd6e1cb29e7700949e8e80e
halesh0905 <halesh0905@gmail.com>
2026-07-27 13:11:29 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,25 +1,10 @@
-"""
-URL configuration for user_auth project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 # user/urls.py
-# project urls
-# user_auth/urls.py
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from .views import RegisterView, LoginView, ProfileView
+
 urlpatterns = [
-path('admin/', admin.site.urls),
-path('api/', include('user.urls')),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
